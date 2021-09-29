ed8300c34676821c0a393e9db0bcc0f91e34be07
Niklas Ertle <hockeytertle@gmail.com>
2021-09-28 21:19:31 -0600

    Updated routes

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,9 +1,32 @@
-// /api/workouts
-// READ ALL workouts
-// CREATE workout
+const router = require('express').Router();
+const {Workout, User, Comment, Category} = require('../../models')
+
+// Get all workouts
+router.get('/', async (req, res) => {
+    try {
+        const workoutData = await Workout.findAll({include: User, Comment, Category});
+        if (!workoutData) {
+            res.status(404).json({ message: 'No workout data found'});
+            return;
+        }
+        res.status(200).json(workoutData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Create a workout
+router.post('/', async (req, res) => {
+    try {
+        const workoutData = await Workout.create(req.body);
+        res.status(200).json({workoutData, message: 'Workout created!'})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // /api/workouts/:id
-// READ a workout
+// Get a single workout by ID
 // UPDATE a  workout
 // DELETE a workout
-
+module.exports = router;
