200ecc40340e23a5ab6d08aa715eb0d58e4960d8
Niklas Ertle <hockeytertle@gmail.com>
2021-10-14 17:31:53 -0600

    Added comments to the API files so that they match formatting

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,11 +4,18 @@ const { Comment } = require("../../models");
 // Creates a comment
 router.post("/", async (req, res) => {
   try {
-    req.body.user_id = req.session.userId;
+    // Sets the user id of the new comment equal to the sessions user id
+    req.body.user_id = req.session.userId;\
+
+    // If the comment is an empty string set to null for a server error to occur
     if (req.body.comment == "") {
       req.body.comment = null;
     }
+
+    // Creates the comment with the req.body
     const commentData = await Comment.create(req.body);
+
+    // Status to say the comment was created successfully
     res.status(200).json({ commentData, message: "Comment created" });
   } catch (err) {
     res.status(500).json(err);
--- controllers/api/likeRoutes.js
+++ controllers/api/likeRoutes.js
@@ -4,8 +4,13 @@ const { Like } = require("../../models");
 // Creates a like
 router.post("/", async (req, res) => {
   try {
+    // Sets the user id of the new like equal to the sessions user id
     req.body.user_id = req.session.userId;
+
+    // Creates the new like with the req.body
     const likeData = await Like.create(req.body);
+
+    // Response saying the like was created
     res.status(200).json({ likeData, message: "Like created" });
   } catch (err) {
     res.status(500).json(err);
@@ -15,10 +20,12 @@ router.post("/", async (req, res) => {
 // Deletes a like
 router.delete("/:id", async (req, res) => {
   try {
+    // Deleetes the like where the workout id and user id match the session user and the workout page id
     await Like.destroy({
       where: { workout_id: req.params.id, user_id: req.session.userId },
     });
 
+    // Response to show the workout was unliked
     res.status(200).json({ message: "Workout unliked" });
   } catch (err) {
     res.status(500).json(err);
